c663b8efa553271ef909c95edf3b5b68f04b457c
kayalraju <87748199+kayalraju@users.noreply.github.com>
2023-06-01 16:22:22 +0530

    first commit

---
+++ route/authRoute.js
@@ -0,0 +1,34 @@
+import express  from "express";
+const router=express.Router();
+import {registerController,
+    loginController,
+    testController,
+    forgrtPasswordController,
+    updateProfileController,
+} from '../controller/authController.js';
+import { isAdmin, requireSigning } from "../middleware/authMiddleware.js";
+
+router.post('/register',registerController);
+router.post('/login',loginController);
+//forgrt password
+router.post('/forgot-password',forgrtPasswordController)
+
+//update profile
+router.put("/profile", requireSigning, updateProfileController);
+
+//protrct route
+router.get('/test',requireSigning,isAdmin,testController)
+
+
+//protected route Auth user
+router.get("/user-auth", requireSigning, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
+//protected route Auth admin
+router.get("/admin-auth", requireSigning,isAdmin,(req, res) => {
+    res.status(200).send({ ok: true });
+  });
+
+
+
+export default router;
---
+++ route/productRoute.js
@@ -0,0 +1,47 @@
+import express from "express";
+const router = express.Router();
+import { isAdmin, requireSigning } from '../middleware/authMiddleware.js'
+import {
+     ProductCountController,
+     ProductFilterController,
+     ProductListController,
+     createProductController,
+     deleteProductController,
+     getProductController,
+     getSingleProductController,
+     productCategoryController,
+     productPhotoController,
+     realtedProductController,
+     searchProductController,
+     updateProductController
+} from "../controller/productController.js";
+import formidable from "express-formidable";
+
+//create category
+router.post("/create-product", requireSigning, isAdmin, formidable(), createProductController)
+//update product
+router.put("/update-product/:pid", requireSigning, isAdmin, formidable(), updateProductController)
+//get products
+router.get("/get-product", getProductController)
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get Photo
+router.get("/product-photo/:pid", productPhotoController);
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+//filter rproduct
+router.post("/filter-product", ProductFilterController);
+//count Product
+router.get("/product-count", ProductCountController);
+// Product par pages
+router.get("/product-list/:page", ProductListController);
+//search product
+router.get("/search/:keyword", searchProductController);
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+
+export default router;
